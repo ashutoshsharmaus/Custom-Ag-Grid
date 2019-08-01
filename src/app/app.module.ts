e0f3ca9c6be6312a7aed6f6f4104bcf65c29df86
zd4eu <ashutosh.sharma@nscorp.com>
2019-08-01 11:16:46 -0400

    bunch of stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgAdvanceGridComponent } from './ns-ag-library/ng-advance-grid/ng-advance-grid.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AgGridModule } from 'ag-grid-angular';
+import 'ag-grid-enterprise';
+import {MatButtonModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgAdvanceGridComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule.withComponents([]),
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
