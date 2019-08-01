a7769621f4eea2a604ef615e9aab7ac41f80cb9e
zd4eu <ashutosh.sharma@nscorp.com>
2019-08-01 13:04:25 -0400

    bunch of other crap

--- src/app/ng-showcase/ng-sample-grid/ng-sample-grid.component.ts
+++ src/app/ng-showcase/ng-sample-grid/ng-sample-grid.component.ts
@@ -6,6 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ng-sample-grid.component.scss']
 })
 export class NgSampleGridComponent implements OnInit {
+  columnDefinition = [
+    {
+      headerName: 'make',
+      field: 'make', sortable: true,
+      filter: true,
+      enableRowGroup: true,
+      // checkboxSelection: true,
+      // rowGroup: true
+    },
+    {
+      headerName: 'model',
+      field: 'model',
+      sortable: true,
+      filter: true,
+      width: 300,
+      enableRowGroup: true,
+    },
+    {
+      headerName: 'price',
+      field: 'price',
+      sortable: true,
+      filter: true,
+      enableRowGroup: true,
+      width: 300
+    }
+  ];
+
+  autoGroupColumnDef = {
+    headerName: 'Model',
+    field: 'model',
+    cellRenderer: 'agGroupCellRenderer',
+    cellRendererParams: {
+      checkbox: true
+    }
+  };
 
   constructor() { }
 
