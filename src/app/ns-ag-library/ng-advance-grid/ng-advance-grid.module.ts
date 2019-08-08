a649c332d2985790a90c30aedf49a0c417322413
zd4eu <ashutosh.sharma@nscorp.com>
2019-08-08 12:30:41 -0400

    removed bunch of other logic

--- src/app/ns-ag-library/ng-advance-grid/ng-advance-grid.module.ts
+++ src/app/ns-ag-library/ng-advance-grid/ng-advance-grid.module.ts
@@ -1,16 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  MatButtonModule,
-  MatCardModule, MatDialogModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule,
-  MatMenuModule, MatOptionModule,
-  MatRadioModule, MatSelectModule, MatTooltipModule
-} from '@angular/material';
 import {NgAdvanceGridComponent} from './ng-advance-grid.component';
-import {DragDropModule} from '@angular/cdk/drag-drop';
 import {NgGridTbarModule} from '../ng-grid-tbar/ng-grid-tbar.module';
 
 
@@ -19,39 +9,10 @@ import {NgGridTbarModule} from '../ng-grid-tbar/ng-grid-tbar.module';
   declarations: [NgAdvanceGridComponent],
   imports: [
     CommonModule,
-    MatButtonModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule,
-    MatInputModule,
-    MatMenuModule,
-    MatRadioModule,
-    MatButtonModule,
-    MatTooltipModule,
-    MatFormFieldModule,
-    MatDialogModule,
-    DragDropModule,
-    MatOptionModule,
-    MatSelectModule,
-    MatFormFieldModule,
     NgGridTbarModule,
   ],
   exports: [
-    MatButtonModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule,
-    MatInputModule,
-    MatMenuModule,
-    MatRadioModule,
-    MatButtonModule,
-    MatTooltipModule,
-    MatFormFieldModule,
-    MatDialogModule,
-    DragDropModule,
-    MatOptionModule,
-    MatSelectModule,
-    MatFormFieldModule,
+    NgGridTbarModule,
     NgAdvanceGridComponent
   ]
 })
