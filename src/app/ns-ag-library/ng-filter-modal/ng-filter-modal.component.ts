1ec1135366ff9934fda8c779cbcbd0f14d752320
zd4eu <ashutosh.sharma@nscorp.com>
2019-08-20 14:00:13 -0400

    filter done.

--- src/app/ns-ag-library/ng-filter-modal/ng-filter-modal.component.ts
+++ src/app/ns-ag-library/ng-filter-modal/ng-filter-modal.component.ts
@@ -4,32 +4,6 @@ import {MatDialogRef} from '@angular/material';
 import {FormControl} from '@angular/forms';
 import {FilterDataSetByCategory} from './FilterDataSetByCategory';
 
-// class FilterCategoryBy {
-//   private filterCategory: string;
-//   private filterCategoryArray: Array<FilterData>;
-//
-//   constructor(filterCategory, filterCategoryArray) {
-//     this.filterCategory = filterCategory;
-//     this.filterCategoryArray = filterCategoryArray;
-//   }
-//
-//   get getFilterCategory(): string {
-//     return this.filterCategory;
-//   }
-//
-//   set setFilterCategory(value: string) {
-//     this.filterCategory = value;
-//   }
-//
-//   get getFilterCategoryArray(): Array<FilterData> {
-//     return this.filterCategoryArray;
-//   }
-//
-//   set setFilterCategoryArray(value: Array<FilterData>) {
-//     this.filterCategoryArray = value;
-//   }
-// }
-
 @Component({
   selector: 'app-ng-filter-modal',
   templateUrl: './ng-filter-modal.component.html',
