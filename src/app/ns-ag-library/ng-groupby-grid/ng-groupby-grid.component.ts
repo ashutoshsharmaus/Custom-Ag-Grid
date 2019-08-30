98f5e5e82364313f68e8aeac33212dbaa4623aaf
zd4eu <ashutosh.sharma@nscorp.com>
2019-08-30 09:02:10 -0400

    group by

--- src/app/ns-ag-library/ng-groupby-grid/ng-groupby-grid.component.ts
+++ src/app/ns-ag-library/ng-groupby-grid/ng-groupby-grid.component.ts
@@ -3,18 +3,6 @@ import {MatDialogRef} from '@angular/material';
 import {FormControl} from '@angular/forms';
 import {ColumnApi} from 'ag-grid-community';
 
-class GroupByObject {
-  groupById: string;
-  columnName: string;
-  groupedBy = false;
-
-  constructor(groupById, columnName, groupedBy) {
-    this.groupById = groupById;
-    this.columnName = columnName;
-    this.groupedBy = groupedBy;
-  }
-}
-
 @Component({
   selector: 'app-ng-groupby-grid',
   templateUrl: './ng-groupby-grid.component.html',
@@ -23,7 +11,6 @@ class GroupByObject {
 export class NgGroupbyGridComponent implements OnInit {
   @Input() gridColumnApi: ColumnApi;
   @ViewChild('groupByRadioGroup', {static: false}) groupByRadioGroup;
-  groupByList: Array<GroupByObject> = [];
   groupByForm = new FormControl('');
 
   constructor(private dialogRef: MatDialogRef<NgGroupbyGridComponent>) {
@@ -31,23 +18,6 @@ export class NgGroupbyGridComponent implements OnInit {
   }
 
   ngOnInit() {
-    debugger;
-    this.populateGroupBy();
-  }
-
-  /**
-   * This method is called in the init of the function.
-   * It will get the list of all the visible columns in the grid and
-   * populate the groupByList object
-   */
-  populateGroupBy() {
-    const columns = this.gridColumnApi.getAllColumns();
-    this.groupByList.push(new GroupByObject(null, 'None', true));
-    for (const column of columns) {
-      if (column.isVisible()) {
-            this.groupByList.push(new GroupByObject(column.getColDef().field, column.getColDef().headerName, column.isRowGroupActive()));
-          }
-    }
   }
 
   /**
