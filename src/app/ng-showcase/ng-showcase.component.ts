a2fbb23c224cd6de5c5e5013d1a50b4335e51fd5
zd4eu <ashutosh.sharma@nscorp.com>
2019-09-07 10:56:51 -0400

    interval added

--- src/app/ng-showcase/ng-showcase.component.ts
+++ src/app/ng-showcase/ng-showcase.component.ts
@@ -6,12 +6,20 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./ng-showcase.component.scss']
 })
 export class NgShowcaseComponent implements OnInit {
-  value = 50;
-  valueHundred = 100;
+  value = 0;
+  valueHundred = 0;
   showValue = false;
   constructor() { }
 
   ngOnInit() {
+  const interval = setInterval(() => {
+    if (this.value < 100) {
+      this.value += 1;
+      this.valueHundred += 1;
+    } else {
+      clearInterval(interval);
+    }
+  }, 50);
   }
 
 }
