2f586c93fa67ebf8f2006dd8f86e79dd9e56e409
zd4eu <ashutosh.sharma@nscorp.com>
2019-09-07 14:29:47 -0400

    more progress bar changes.

--- src/app/ng-showcase/ng-showcase.component.ts
+++ src/app/ng-showcase/ng-showcase.component.ts
@@ -7,7 +7,8 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NgShowcaseComponent implements OnInit {
   leftValue = 0;
-  centerValue = 0;
+  centerValue1 = 0;
+  centerValue2 = 0;
   rightValue = 0;
   showValue = false;
   constructor() { }
@@ -16,9 +17,11 @@ export class NgShowcaseComponent implements OnInit {
   const interval = setInterval(() => {
     if (this.leftValue < 100) {
       this.leftValue ++;
-    } else if (this.leftValue === 100 && this.centerValue < 100) {
-      this.centerValue ++;
-    } else if (this.leftValue === 100 && this.centerValue === 100 && this.rightValue < 100) {
+    } else if (this.leftValue === 100 && this.centerValue1 < 100) {
+      this.centerValue1 ++;
+    } else if (this.leftValue === 100 && this.centerValue1 === 100 && this.centerValue2 < 100) {
+      this.centerValue2 ++;
+    } else if (this.leftValue === 100 && this.centerValue1 === 100 && this.rightValue < 100) {
       this.rightValue ++;
     } else {
       clearInterval(interval);
